6f80bf44e541fb08787fc8cb9b6c6751cb12403f
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-05-13 23:44:13 -0400

    add palomaconbotas mail

--- mailer.js
+++ mailer.js
@@ -2,14 +2,14 @@ const nodemailer = require('nodemailer')
 // Paso 1
 function enviar(to, subject, text) {
   let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: 'yahoo',
     auth: {
-      user: 'oviedolopezadriana13@gmail.com',
-      pass: '6070380717',
+      user: 'palomaconbotas@yahoo.com',
+      pass: 'kddpoixqhfdwfoqj',
     },
   })
   let mailOptions = {
-    from: 'oviedolopezadriana13@gmail.com',
+    from: 'palomaconbotas@yahoo.com',
     to,
     subject,
     text,
