9601d7710d99961c3d61f729a2421d8a06676391
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-05-13 23:54:47 -0400

    update

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ const getData = require('./getdata')
 http
   .createServer(async (req, res) => {
     let { correos, asunto, contenido } = url.parse(req.url, true).query
-    if (req.url == '/') {
+    if (req.url === '/') {
       res.setHeader('content-type', 'text/html')
       fs.readFile('index.html', 'utf8', (err, data) => {
         res.end(data)
       })
     }
     if (req.url.startsWith('/mailing')) {
-      const {valorDolar,valorEuro,valorUf,valorUtm} = await getData()
-      const mensaje = `\nEl valor del dolar el dia de hoy es : ${valorDolar}\nEl valor del euro el dia de hoy es : ${valorEuro}\nEl valor del UF el dia de hoy es : ${valorUf}\nEl valor del UTM el dia de hoy es : ${valorUtm}`
+      const mensaje = await getData()
       if (correos) {
         enviar(correos.split(','), asunto, contenido + mensaje)
-        fs.writeFile(`./correos/${uuidv4()}.txt`, `${contenido + mensaje}`, (err) => {
-          if (err) console.log(err)
-          else console.log('Archivo creado!')
-        })
+        fs.writeFile(
+          `./correos/${uuidv4()}.txt`,
+          `${contenido + mensaje}`,
+          (err) => {
+            if (err) console.log(err)
+            else console.log('Archivo creado!')
+          }
+        )
         res.end('Correo enviado exitosamente!')
       } else {
         res.end('Debes ingresar algun correo de destino')
--- getdata.js
+++ getdata.js
@@ -5,7 +5,9 @@ const getData = async () => {
   const valorEuro = data.euro.valor
   const valorUf = data.uf.valor
   const valorUtm = data.utm.valor
-  return{valorDolar,valorEuro,valorUf,valorUtm}
+  return  `\nEl valor del dolar el dia de hoy es : ${valorDolar}\nEl valor del euro el dia de hoy es : ${valorEuro}\nEl valor del UF el dia de hoy es : ${valorUf}\nEl valor del UTM el dia de hoy es : ${valorUtm}`
+ 
+  
 }
 
 module.exports = getData
