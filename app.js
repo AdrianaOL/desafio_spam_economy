1cd35e225bb6ea09bd0c14b4171c1f083386bb87
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-05-16 19:05:38 -0400

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const url = require('url')
 const http = require('http')
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
-const enviar = require('./mailer')
+const mailer = require('./mailer')
 const getData = require('./getdata')
 http
   .createServer(async (req, res) => {
@@ -14,21 +14,25 @@ http
       })
     }
     if (req.url.startsWith('/mailing')) {
-      const mensaje = await getData()
       if (correos) {
-        enviar(correos.split(','), asunto, contenido + mensaje)
-        fs.writeFile(
-          `./correos/${uuidv4()}.txt`,
-          `${contenido + mensaje}`,
-          (err) => {
-            if (err) console.log(err)
-            else console.log('Archivo creado!')
-          }
-        )
-        res.end('Correo enviado exitosamente!')
+        const mensaje = await getData()
+        mailer(correos.split(','), asunto, contenido + mensaje).then((data)=>{
+          console.log(data)
+          fs.writeFile(
+            `./correos/${uuidv4()}.txt`,
+            `${contenido + mensaje}`,
+            (err) => {
+              if (err) console.log(err)
+              else console.log('Archivo creado!')
+            }
+          )
+         
+        }).catch((err)=>{res.end(`error al enviar correo: ${err}`)})
       } else {
         res.end('Debes ingresar algun correo de destino')
       }
+      
+      res.end('correo enviado exitosamente!') 
     }
   })
   .listen(3000)
--- mailer.js
+++ mailer.js
@@ -1,23 +1,24 @@
 const nodemailer = require('nodemailer')
-// Paso 1
-function enviar(to, subject, text) {
-  let transporter = nodemailer.createTransport({
-    service: 'yahoo',
-    auth: {
-      user: 'palomaconbotas@yahoo.com',
-      pass: 'kddpoixqhfdwfoqj',
-    },
-  })
-  let mailOptions = {
-    from: 'palomaconbotas@yahoo.com',
-    to,
-    subject,
-    text,
-  }
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) console.log(err)
-    if (data) console.log(data)
+function mailer(to, subject, text) {
+  return new Promise((resolve, reject) => {
+    const transporter = nodemailer.createTransport({
+      service: 'yahoo',
+      auth: {
+        user: 'palomaconbotas@yahoo.com',
+        pass: 'kddpoixqhfdwfoqj',
+      },
+    })
+    const mailOptions = {
+      from: 'palomaconbotas@yahoo.com',
+      to,
+      subject,
+      text,
+    }
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) reject(err)
+      if (data) resolve(data)
+    })
   })
 }
-// Paso 2
-module.exports = enviar
+
+module.exports = mailer
